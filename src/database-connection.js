db2d53281090b287d3754a40b64a1a8451426d26
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-07 10:45:39 +0000

    Edit by my project

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // eslint-disable-next-line prefer-const
-let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
+let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongo:27017' // before:'mongodb://localhost:27017'
 
 mongoose.set('debug', true)
 
@@ -10,5 +10,5 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('connection established'))
+  .then(() => console.log('connection established', connectionString))
   .catch(error => console.log('not connected:', error))
