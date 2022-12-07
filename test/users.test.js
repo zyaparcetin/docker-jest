be7e5e98b565c1c2b1c43dcd4db2827e6299cb4b
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-07 16:52:00 +0000

    Add some comment lines

--- test/users.test.js
+++ test/users.test.js
@@ -5,6 +5,30 @@ describe('User endpoints', () => {
   it('POST request to /users should create a user', async () => {
     const userToCreate = {
       name: `Somename${Date.now()}`,
+      age: 12,
+    }
+
+    const createdUser = (await request(app).post('/users').send(userToCreate)).body
+
+    expect(createdUser.name).toBe(userToCreate.name)
+    expect(createdUser.age).toBe(userToCreate.age)
+  })
+
+  it('GET request to /users should list users', async () => {
+    const userList = (await request(app).get('/users')).body
+    const userExist = userList.length > 0
+
+    expect(userExist).toBe(true)
+  })
+})
+
+/* const request = require('supertest')
+const app = require('../src/app')
+
+describe('User endpoints', () => {
+  it('POST request to /users should create a user', async () => {
+    const userToCreate = {
+      name: `Somename${Date.now()}`,
       age: 27,
       bio: 'Been there. Done that.',
     }
@@ -23,3 +47,4 @@ describe('User endpoints', () => {
     expect(userExist).toBe(true)
   })
 })
+*/
